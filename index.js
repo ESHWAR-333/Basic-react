d8bccc4ae0bb5cf21eb841e2e8d36cb3b420fe8f
Eshwar <chalamallan3@gmail.com>
2023-04-05 17:46:12 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const element1 = (
+  <div className="root">
+    <h1 className="greeting">Hello World!</h1>
+    <p>Namasthe!</p>
+  </div>
+);
+ReactDOM.render(element1, document.getElementById("root"));
